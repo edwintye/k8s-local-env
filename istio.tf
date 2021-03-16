271531a8166b257a5cb6fa24202534e5216a2358
Edwin Tye <edwin.tye@mastercard.com>
2021-03-16 10:09:34 +0000

    also output the kiali ui location

--- istio.tf
+++ istio.tf
@@ -8,27 +8,43 @@ resource "null_resource" "istio" {
   provisioner "local-exec" {
     command = "istioctl install -f istio/custom-istio.yaml --set profile=minimal"
   }
-  depends_on = [kubernetes_namespace.istio_system]
+  depends_on = [
+    kubernetes_namespace.istio_system
+  ]
 }
 
 resource "kubectl_manifest" "istio-app-mtls" {
   yaml_body = templatefile("istio/peer-auth.yaml", {
     applicationNamespace = kubernetes_namespace.app.metadata[0].name
   })
-  depends_on = [null_resource.istio, kubernetes_namespace.app]
+  depends_on = [
+    null_resource.istio,
+    kubernetes_namespace.app
+  ]
+
 }
 
 resource "helm_release" "kiali" {
-  name       = "kiali"
+  name = "kiali"
   repository = "https://kiali.org/helm-charts"
-  chart      = "kiali-server"
-  version    = "v1.29.0"
+  chart = "kiali-server"
+  version = "v1.29.0"
 
   values = [
     file("istio/kiali-values.yaml")
   ]
 
   namespace = kubernetes_namespace.istio_system.metadata[0].name
-  depends_on = [kubernetes_namespace.istio_system]
+  depends_on = [
+    kubernetes_namespace.istio_system
+  ]
   wait = true
 }
+
+output "kiali-ui" {
+  value = "http://${helm_release.kiali.name}.${helm_release.kiali.namespace}:20001/kiali"
+  depends_on = [
+    helm_release.kiali
+  ]
+}
+
