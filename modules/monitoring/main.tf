487208cc546f1078aa8a6555aa449e1b92340df3
Edwin Tye <edwin.tye@mastercard.com>
2021-03-17 20:30:49 +0000

    allow more flexible dependency

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.0.2"
+      version = ">=2.0.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.0.3"
+      version = ">=2.0.0"
     }
   }
 }
