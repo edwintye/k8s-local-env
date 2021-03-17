4e3c9029206d93686c3ba7d3f6d2bdf1bbfa01ed
Edwin Tye <edwin.tye@mastercard.com>
2021-03-17 19:24:19 +0000

    move istio related stuff into a module

--- modules/istio/main.tf
+++ modules/istio/main.tf
@@ -1,3 +1,34 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.1.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_context = var.kube_context
+  config_path    = var.kube_config
+}
+
+provider "helm" {
+  kubernetes {
+    config_context = var.kube_context
+    config_path    = var.kube_config
+  }
+}
+
 resource "kubernetes_namespace" "istio_system" {
   metadata {
     name = "istio-system"
@@ -6,7 +37,7 @@ resource "kubernetes_namespace" "istio_system" {
 
 resource "null_resource" "istio" {
   provisioner "local-exec" {
-    command = "istioctl install -f istio/custom-istio.yaml --set profile=minimal"
+    command = "istioctl install -f ${path.module}/custom-istio.yaml --set profile=minimal --skip-confirmation"
   }
   depends_on = [
     kubernetes_namespace.istio_system
@@ -14,7 +45,7 @@ resource "null_resource" "istio" {
 }
 
 resource "kubectl_manifest" "istio-app-mtls" {
-  yaml_body = templatefile("istio/peer-auth.yaml", {
+  yaml_body = templatefile("${path.module}/peer-auth.yaml", {
     applicationNamespace = kubernetes_namespace.app.metadata[0].name
   })
   depends_on = [
@@ -30,7 +61,7 @@ resource "helm_release" "kiali" {
   version    = "v1.29.0"
 
   values = [
-    file("istio/kiali-values.yaml")
+    file("${path.module}/kiali-values.yaml")
   ]
 
   namespace = kubernetes_namespace.istio_system.metadata[0].name
